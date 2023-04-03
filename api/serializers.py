9a699f8563318619ebea177ec3f43dbbff7241b7
Roman Popov <i@romanpopoff.ru>
2023-04-03 16:57:33 +0300

    init django+poetry project

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from api.models import DataMap
-
-class DataMapSer(ModelSerializer):
-    class Meta:
-        model = DataMap
-        fields = ['address', 'geoinfo', 'parks', 'stores', 'shools', 'kindergartens']
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.search),
-    # path('posts/<int:pk>/', views.DataMapDetail.as_view()),
-]
